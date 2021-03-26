bd7031f7817fd113ad9708560677871f52839833
alvid98 <alvid.1998@gmail.com>
2021-03-26 19:47:22 +0100

    Añadida funcion de exportar BBDD

--- scripts/debian/mysqldebian.sh
+++ scripts/debian/mysqldebian.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 salir=0
 while [ $salir -eq 0 ]; do
     clear
@@ -10,7 +9,7 @@ while [ $salir -eq 0 ]; do
 	echo "┌---------------------------------------┐"
 	echo "|1-Importar BBDD                        |"
 	echo "├---------------------------------------┤"
-    echo "|2-Exportar BBDD                        |"
+	echo "|2-Exportar BBDD                        |"
 	echo "└---------------------------------------┘"
 	echo "╔═══════════════════════════════════════╗"
 	echo "║3-Salir                                ║"
@@ -18,14 +17,38 @@ while [ $salir -eq 0 ]; do
 	read -p "Opción: " valor
 	case $valor in
 		1)
-        clear
-        echo "╔═══════════════════════════════════════╗"
-	    echo "║Importar/Exportar BBDD                 ║"
-	    echo "╚═══════════════════════════════════════╝"
-        mysql -u root -e 'show databases;'
 		;;
 		2)
-        
+		salir2=0
+		while [ $salir2 -eq 0 ]; do
+        		clear
+        		echo "╔═══════════════════════════════════════╗"
+			echo "║Exportar BBDD                          ║"
+			echo "║Seleccione la BBDD que desea exportar. ║"
+			echo "╚═══════════════════════════════════════╝"
+        		lista=$(mysql -u root -e 'show databases;' | sed '1d')
+			num=1
+			echo "┌---------------------------------------┐"
+
+			for x in $lista; do
+				echo "| "$num"-"$x
+				num=$[$num+1]
+			done
+
+			echo "└---------------------------------------┘"
+			echo "╔═══════════════════════════════════════╗"
+        		echo "║$num-Salir                                ║"
+        		echo "╚═══════════════════════════════════════╝"
+			read -p "Opción: " BBDD
+
+			if [ "$BBDD" -lt "$num" ] && [ "$BBDD" -gt 0 ]; then
+				NOMBRE=$( echo "$lista" | cut -d$'\n' -f$BBDD )
+				mysqldump -u root -p $NOMBRE > ~/$NOMBRE.sql
+				read -p "Se ha exportado la base de datos en $HOME"
+			elif [ "$BBDD" -eq "$num" ]; then
+				salir2=1
+			fi
+		done
 		;;
 		3)
 		salir=1

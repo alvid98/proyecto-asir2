447ab2b0932e361e949f9705e7308a0e82c18ba5
alvid98 <alvid.1998@gmail.com>
2021-06-04 16:40:32 +0200

    Eliminando entrada

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -5,7 +5,7 @@
 
 salir=0
 while [ $salir -eq 0 ]; do
-valor=$(dialog --nocancel --backtitle "Proyecto asir" --title "Sistema "$1":" --stdout --menu "Elija una opcion" 0 0 0 1 "Instalar paquetes" 2 "Desinstalar paquetes" 3 "Actualizar Paquetes" 4 "Configuracion de VirtualHosts" 5 "Instalacion de certificados" 6 "Importar/Exportar BBDD" 7 "Copias de seguridad" 8 "Salir")
+valor=$(dialog --nocancel --backtitle "Proyecto asir" --title "Sistema "$1":" --stdout --menu "Elija una opcion" 0 0 0 1 "Instalar paquetes" 2 "Desinstalar paquetes" 3 "Actualizar Paquetes" 4 "Configuracion de VirtualHosts" 5  "Importar/Exportar BBDD" 6 "Copias de seguridad" 7 "Salir")
 	clear
 
 	case $valor in
@@ -22,15 +22,12 @@ valor=$(dialog --nocancel --backtitle "Proyecto asir" --title "Sistema "$1":" --
 		./scripts/vhosts.sh
 		;;
 		5)
-
-		;;
-		6)
 		./scripts/mysql.sh
 		;;
-		7)
+		6)
 		./scripts/copiaseg.sh
 		;;
-		8)
+		7)
 		dialog --stdout --title "Confirmación" --yesno "¿Seguro que desea salir?" 0 0
 		if [[ $? -eq 0 ]]; then
 		salir=1
